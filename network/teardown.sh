0aff30e79728e5ca11a7b38828193e1512b53ada
maheshwarishikha <maheshwari.shikha@gmail.com>
2018-04-23 08:29:58 +0000

    Update teardown.sh

--- network/teardown.sh
+++ network/teardown.sh
@@ -5,6 +5,7 @@ set -e
 
 # Shut down the Docker containers for the system tests.
 docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.yml down
+docker rm -f $(docker ps -aq)
 
 # remove chaincode docker images
 docker rmi $(docker images dev-* -q)
